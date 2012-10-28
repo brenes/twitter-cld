387cf96492dc7125f31aa167a1545bedf9510355
David J. Brenes <davidjbrenes@gmail.com>
2012-10-28 13:32:33 +0100

    Added a top list gauge for popular languages

--- lib/fnord_metric_app.rb
+++ lib/fnord_metric_app.rb
@@ -12,12 +12,20 @@ class FnordMetricApp
       :series => [:reliable, :not_reliable],
       :punchcard => true
 
+    toplist_gauge :popular_languages,
+      :title => "Detected languages",
+      :resolution => 10.seconds
+
     event(:tweet_processed) do
+
       if data[:reliable]
         incr :cld_reliability, :reliable
+        observe :popular_languages, data[:language]
       else
         incr :cld_reliability, :not_reliable
       end
+
+
     end
 
   end

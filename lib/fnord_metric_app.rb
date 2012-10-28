9e1b22de27eef326000860442d0e3d87cdd2b7af
David J. Brenes <davidjbrenes@gmail.com>
2012-10-28 18:51:10 +0100

    Introducing some other metrics in fnord app

--- lib/fnord_metric_app.rb
+++ lib/fnord_metric_app.rb
@@ -6,6 +6,8 @@ class FnordMetricApp
 
   FnordMetric.namespace :myapp do
 
+    hide_active_users
+
     timeseries_gauge :cld_reliability,
       :resolution => 10.seconds,
       :title => "Reliability of CLD",
@@ -13,14 +15,30 @@ class FnordMetricApp
       :punchcard => true
 
     toplist_gauge :popular_languages,
-      :title => "Detected languages",
+      :title => "Popular languages",
       :resolution => 10.seconds
 
+    gauge :processed_tweets,
+      :tick => 10.seconds,
+      :title => "Processed Tweets"
+
+    widget 'Performance', {
+        :title => "Performance statistics",
+        :type => :timeline,
+        :width => 100,
+        :gauges => :processed_tweets,
+        :include_current => true,
+        :autoupdate => 30
+      }
+
     event(:tweet_processed) do
+
+      incr :processed_tweets
 
       if data[:reliable]
         incr :cld_reliability, :reliable
         observe :popular_languages, data[:language]
+        incr_field :detected_languages, data[:language], 1
       else
         incr :cld_reliability, :not_reliable
       end
